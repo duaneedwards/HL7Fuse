3c40e886e3121af54df713d6c76ea1aeae66895a
Duane Edwards <mail@duaneedwards.net>
2016-06-09 11:31:54 +1000

    Add a new app.config property to specify a directory to where messages recieved that are unable to be parsed by nHapi will be saved to along with the exception that was raised.

--- HL7Fuse/Protocol/HL7RequestInfoParser.cs
+++ HL7Fuse/Protocol/HL7RequestInfoParser.cs
@@ -9,67 +9,94 @@ using NHapiTools.Base.Parser;
 using NHapiTools.Base.Validation;
 using NHapi.Base.Model;
 using System.Configuration;
+using System.IO;
 
 namespace HL7Fuse.Protocol
 {
-    public class HL7RequestInfoParser : IRequestInfoParser<HL7RequestInfo>
-    {
-        #region Private properties
-        private bool HandleEachMessageAsEvent
-        {
-            get
-            {
-                bool result = false;
-                if (!bool.TryParse(ConfigurationManager.AppSettings["HandleEachMessageAsEvent"], out result))
-                    result = false;
+   public class HL7RequestInfoParser : IRequestInfoParser<HL7RequestInfo>
+   {
+      #region Private properties
 
-                return result;
-            }
-        }
-        #endregion
+      private bool HandleEachMessageAsEvent
+      {
+         get
+         {
+            bool result = false;
+            if (!bool.TryParse(ConfigurationManager.AppSettings["HandleEachMessageAsEvent"], out result))
+               result = false;
 
-        #region Public methods
-        /// <summary>
-        /// Parse the message to a RequestInfo class
-        /// </summary>
-        /// <param name="message"></param>
-        /// <returns></returns>
-        public HL7RequestInfo ParseRequestInfo(string message)
-        {
-            return ParseRequestInfo(message, string.Empty);
-        }
+            return result;
+         }
+      }
 
-        public HL7RequestInfo ParseRequestInfo(string message, string protocol)
-        {
-            HL7RequestInfo result = new HL7RequestInfo();
-            PipeParser parser = new PipeParser();
+      #endregion
 
-            try
-            {
-                ConfigurableContext configContext = new ConfigurableContext(parser.ValidationContext);
-                parser.ValidationContext = configContext;
-            }
-            catch
-            {
-                // Ignore any error, since the config is probably missing
-            }
+      #region Public methods
 
+      /// <summary>
+      /// Parse the message to a RequestInfo class
+      /// </summary>
+      /// <param name="message"></param>
+      /// <returns></returns>
+      public HL7RequestInfo ParseRequestInfo(string message)
+      {
+         return ParseRequestInfo(message, string.Empty);
+      }
+
+      public HL7RequestInfo ParseRequestInfo(string message, string protocol)
+      {
+         HL7RequestInfo result = new HL7RequestInfo();
+         PipeParser parser = new PipeParser();
+
+         try
+         {
+            ConfigurableContext configContext = new ConfigurableContext(parser.ValidationContext);
+            parser.ValidationContext = configContext;
+         }
+         catch
+         {
+            // Ignore any error, since the config is probably missing
+         }
+
+         try
+         {
             IMessage hl7Message = parser.Parse(message);
 
             result = new HL7RequestInfo();
             if (HandleEachMessageAsEvent)
-                result.Key = "V" + hl7Message.Version.Replace(".", "") + "." + hl7Message.GetStructureName();
+               result.Key = "V" + hl7Message.Version.Replace(".", "") + "." + hl7Message.GetStructureName();
             else
-                result.Key = "V" + hl7Message.Version.Replace(".", "") + ".MessageFactory";
+               result.Key = "V" + hl7Message.Version.Replace(".", "") + ".MessageFactory";
 
             if (!string.IsNullOrEmpty(protocol))
-                result.Key += protocol;
+               result.Key += protocol;
 
             result.Message = hl7Message;
 
             // Parse the message
             return result;
-        }
-        #endregion
-    }
+         }
+         catch (Exception ex)
+         {
+            var location = ConfigurationManager.AppSettings["ParseErrorLogDirectory"];
+            if (!string.IsNullOrEmpty(location))
+            {
+               if (!Directory.Exists(location))
+               {
+                  Directory.CreateDirectory(location);
+               }
+
+               string filename = string.Format("{0}_{1}_{2}", DateTime.Now.ToString("yyyyMMdd"), DateTime.Now.ToString("HHmmssfff"), Guid.NewGuid());
+               string fullPath = Path.Combine(location, filename);
+               File.WriteAllText(fullPath + ".HL7", message);
+
+               string errorMessage = ex.ToString();
+               File.WriteAllText(fullPath + ".log", errorMessage);
+            }
+            throw;
+         }
+      }
+
+      #endregion
+   }
 }
