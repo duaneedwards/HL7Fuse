9dc2b8f5ebbbee4e5418de1526562ac91587d9ef
Duane Edwards <mail@duaneedwards.net>
2016-08-29 15:14:08 +1000

    add new MessageFactoryFile / MessageFactoryMLLP for new HL7 versions

---
+++ HL7Fuse.Hub/V28/MessageFactoryMLLP.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HL7Fuse.Hub.V28
+{
+    /// <summary>
+    /// Message factory
+    /// </summary>
+    public class MessageFactoryMLLP : MessageFactoryBaseMLLP
+    {
+        #region Public properties
+        public override string Name
+        {
+            get { return "V28.MessageFactoryMLLP"; }
+        }
+        #endregion
+    }
+}
